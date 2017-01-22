558a095720fd9bdab0fb85c3fb982086b67af431
Ricardo Magalhães <magalhini@gmail.com>
2017-01-22 16:37:21 +0100

    Refactored views and styles before styling begins

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const engines = require('consolidate')
 const goodreads = require('goodreads')
 const bodyParser = require('body-parser')
 const env = require('node-env-file')
+const PORT = process.env.PORT || 3000;
 
 // load environment variables
 env(__dirname + '/.env');
@@ -17,6 +18,7 @@ const goodreadsClient = new goodreads.client({
 app.engine('hbs', engines.handlebars)
 app.set('views', './views')
 app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/'))
 app.use(bodyParser.urlencoded({ extended: true }))
 
 function getCurrentlyReading(userID) {
@@ -28,7 +30,7 @@ function getCurrentlyReading(userID) {
     }, (data) => {
       if (typeof data.html !== 'object') {
         const { books } = data.GoodreadsResponse
-        console.dir(books[0].book[0].authors[0].author.name);
+        console.dir(books[0].book[0]);
         resolve(books[0].book)
       } else {
         reject({ success: false })
@@ -39,20 +41,22 @@ function getCurrentlyReading(userID) {
 
 app.get('/reading/:user', (req, res) => {
   const user = req.params.user
+
   getCurrentlyReading(user)
-    .then(books => res.render('reading', { books: books }))
+    .then(books => res.render('reading', { books }))
     .catch(err => res.send(err))
 })
 
 app.get('/reading/:user/json', (req, res) => {
   const user = req.params.user
+
   getCurrentlyReading(user)
     .then(books => res.json(books))
     .catch(err => res.send(err))
 })
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'))
+  res.render('index')
 })
 
-app.listen(3000, () => console.log('Listening on http://localhost:3000'))
+app.listen(PORT, () => console.log('Listening on http://localhost:3000'))
